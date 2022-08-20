1704b97f315d8d30629a26ee33ad2154c25f5985
Aqib-Malik <hmaqib92@gmail.com>
2022-08-19 19:18:31 -0700

    first commit

---
+++ lib/widgets/FoodDetailsSlider.dart
@@ -0,0 +1,37 @@
+// import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/material.dart';
+
+class FoodDetailsSlider extends StatelessWidget {
+  String slideImage1;
+  String slideImage2;
+  String slideImage3;
+
+  FoodDetailsSlider(
+      {Key? key,
+      required this.slideImage1,
+      required this.slideImage2,
+      required this.slideImage3})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 10, right: 10),
+      child: Container(
+              child:Image.asset(
+                slideImage1,
+              ),
+              /* Image.asset(
+                slideImage2,
+                height: double.infinity,
+                width: double.infinity,
+              ),
+              Image.asset(
+                slideImage3,
+                height: double.infinity,
+                width: double.infinity,
+              ),*/
+            ),
+    );
+  }
+}
